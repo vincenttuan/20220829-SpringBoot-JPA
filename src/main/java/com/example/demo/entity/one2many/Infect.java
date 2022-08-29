7a12d72f37ee7292592eafbe88b6dacf42338425
teacher <teacher@BE-403->
2022-08-29 21:33:35 +0800

    sts

--- src/main/java/com/example/demo/entity/one2many/Infect.java
+++ src/main/java/com/example/demo/entity/one2many/Infect.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity.one2many;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+// 單向 1 對 多
 @Entity
 @Table(name = "infect")
 public class Infect { // 傳染病
@@ -18,6 +24,33 @@ public class Infect { // 傳染病
 	@Column(nullable = false, length = 50)
 	private String name;
 	
+	@OneToMany // 一對多
+	@JoinColumn(name = "infect_id")
+	private Set<Vaccine> vaccines = new LinkedHashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Vaccine> getVaccines() {
+		return vaccines;
+	}
+
+	public void setVaccines(Set<Vaccine> vaccines) {
+		this.vaccines = vaccines;
+	}
 	
 	
 }
