f7646f31bfdcf215d04547c66cba252d72e9ebbf
teacher <teacher@BE-403->
2022-08-29 20:38:18 +0800

    sts

---
+++ src/main/java/com/example/demo/entity/one2one/Department.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity.one2one;
+
+public class Department {
+
+}
---
+++ src/main/java/com/example/demo/entity/one2one/Manager.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity.one2one;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "manager")
+public class Manager {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String name;
+	
+	private Department department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
+	
+}
