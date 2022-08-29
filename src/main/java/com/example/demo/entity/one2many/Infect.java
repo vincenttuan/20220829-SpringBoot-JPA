1faa2927ecc0cee15b309b46499ca2381367928d
teacher <teacher@BE-403->
2022-08-29 21:29:54 +0800

    sts

---
+++ src/main/java/com/example/demo/entity/one2many/Infect.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity.one2many;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "infect")
+public class Infect { // 傳染病
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String name;
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/entity/one2many/Vaccine.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity.one2many;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vaccine")
+public class Vaccine { // 疫苗
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String name;
+	
+	@Column(columnDefinition = "integer default 1")
+	private Integer count; // 預設要打 1 濟
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	
+	
+}
