32ae038a011d94d4a2891a3abf00e5af102a6e16
teacher <teacher@BE-403->
2022-09-02 19:26:04 +0800

    spring

--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.one2many.twoway.MenuGroup;
+import com.example.demo.entity.one2many.twoway.MenuItem;
 
 @Repository
-public interface MenuItemRepository extends JpaRepository<MenuGroup, Long> {
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 	
 }
