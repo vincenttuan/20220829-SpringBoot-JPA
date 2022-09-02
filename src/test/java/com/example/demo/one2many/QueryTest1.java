42978beb78f0c622bea88b1c21004f5d4f4a5a28
teacher <teacher@BE-403->
2022-09-02 20:00:41 +0800

    spring

--- src/test/java/com/example/demo/one2many/QueryTest1.java
+++ src/test/java/com/example/demo/one2many/QueryTest1.java
@@ -1,5 +1,7 @@
 package com.example.demo.one2many;
 
+import javax.persistence.FetchType;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +23,8 @@ public class QueryTest1 {
 	public void test() {
 		MenuItem m1 = menuItemRepository.findById(1L).get();
 		System.out.println(m1.getName());
-		
+		// 注意: fetch = FetchType.LAZY 預設不加載, 但若執行時期需要實施加載策略, 必須在 application.yml 中加入
+		// enable_lazy_load_no_trans: true
 		System.out.println(m1.getMenuGroup().getName());
 		
 	}
