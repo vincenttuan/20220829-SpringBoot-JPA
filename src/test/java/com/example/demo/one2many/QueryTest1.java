8238c5c1dac6eabc75d3a046e9edd1094db55bbe
teacher <teacher@BE-403->
2022-09-02 19:46:17 +0800

    spring

---
+++ src/test/java/com/example/demo/one2many/QueryTest1.java
@@ -0,0 +1,25 @@
+package com.example.demo.one2many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entity.one2many.twoway.MenuItem;
+import com.example.demo.repository.MenuGroupRepository;
+import com.example.demo.repository.MenuItemRepository;
+
+@SpringBootTest
+public class QueryTest1 {
+	
+	@Autowired
+	MenuGroupRepository menuGroupRepository;
+	
+	@Autowired
+	MenuItemRepository menuItemRepository;
+	
+	@Test
+	public void test() {
+		MenuItem m1 = menuItemRepository.findById(1L).get();
+		System.out.println(m1.getName());
+	}
+}
