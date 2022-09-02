cd4f090235e681560bc833f8de3af90b0b4ebc40
teacher <teacher@BE-403->
2022-09-02 21:32:00 +0800

    spring

--- src/main/java/com/example/demo/entity/many2many/Course.java
+++ src/main/java/com/example/demo/entity/many2many/Course.java
@@ -1,5 +1,63 @@
 package com.example.demo.entity.many2many;
 
-public class Course {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	
+	private Set<Student> students = new LinkedHashSet<>();
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", students=" + students + "]";
+	}
+	
+	
 }
