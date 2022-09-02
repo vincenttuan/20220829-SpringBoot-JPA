1e221623242d405ff9a106b55e0577686ee201e2
teacher <teacher@BE-403->
2022-09-02 20:42:40 +0800

    spring

--- src/main/java/com/example/demo/entity/many2one/oneway/Customer.java
+++ src/main/java/com/example/demo/entity/many2one/oneway/Customer.java
@@ -1,5 +1,70 @@
 package com.example.demo.entity.many2one.oneway;
 
-public class Customer {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column(columnDefinition = "INTEGER DEFAULT 18")
+	private Integer age;
+	
+	@Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+	@Temporal(TemporalType.TIMESTAMP) // 存放的格式: 時間(TIME), 日期(DATE), 時間+日期(TIMESTAMP)
+	private Date create = new Date();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getCreate() {
+		return create;
+	}
+
+	public void setCreate(Date create) {
+		this.create = create;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", age=" + age + ", create=" + create + "]";
+	}
+	
+	
+	
 }
